1b6712e095b88792cf5d048c72a025172f753bd7
Akatsuki2555 <akatsuki2555@proton.me>
2024-06-25 17:36:48 +0200

    Rewrite the mod, remove build files

---
+++ Mods/OldCarSounds/RPMGauge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GoodOldMSC.Mods.OldCarSounds {
+
+    public class RPMGauge : MonoBehaviour {
+
+        private void Start() {
+            if ((bool)OldCarSounds.OldRpmGaugeSettings.Value) {
+                GameObject o = transform.FindChild("Pivot/needle").gameObject;
+                o.transform.localScale = new Vector3(0.64f, 1, 0.8f);
+            }
+        }
+    }
+}
